79f1165415ea457083ef95d0a10c36cf985d8f60
John Asatryan <john.asatryan@gmail.com>
2020-12-29 23:40:47 +0400

    message

--- firstTask.cpp
+++ firstTask.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-    int reverse(int number) {
+int reverse(int number) {
     int reverseNumber = 0;
     while(number != 0) {
         reverseNumber = (reverseNumber * 10) +  number % 10;
---
+++ fourteenthTask.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+char arr[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+void deci_hexo (int number, int system) {
+	if(number > 0) {
+		int tmp = number / system;
+		deci_hexo(tmp, system);
+		std::cout << arr[number % system];
+	}
+}
+int main() {
+	int number, system;
+	std::cout << "Enter the number to conver: ";
+	std::cin >> number;
+        std::cout << "Enter system (heximal or binary) : ";
+	std::cin >> system;
+	deci_hexo(number, system);
+	std::cout << std::endl;
+	return 0;
+} 
