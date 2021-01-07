1136719a97434f10b96311242bbaf2d0526df098
John Asatryan <john.asatryan@gmail.com>
2021-01-07 21:15:49 +0400

    Initial commit

---
+++ nineteenthTask.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+void rev_elements(std::string arr) {
+	for(int i = arr.size() -1; i>= 0; --i) {
+		std::cout << arr[i];
+	}
+	std::cout << std:: endl;
+}
+
+int main() {
+	std::cout << " Enter elements of an array: ";
+	std::string word;
+	getline(std::cin, word);
+	rev_elements(word);
+	return 0;
+}
